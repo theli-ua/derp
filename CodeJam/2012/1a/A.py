141ef4cb8a79def79cc4efb1a08e2360aff9969e
Anton Romanov <theli.ua@gmail.com>
2013-02-01 16:44:43 +0200

    2012 1a-a done

--- CodeJam/2012/1a/A.py
+++ CodeJam/2012/1a/A.py
@@ -9,23 +9,23 @@ def mul(a,b):
 for casei in xrange(casenum):
     A,B = [int(x) for x in stdin.readline().split()]
     probabilities = [float(x) for x in stdin.readline().split()]
+    probabilities.insert(0,1.0)
 
     #right away
     res = (2 + B)
 
-    #delete all
-    res = min(res, B + A + 1)
-    probability_good = reduce(mul, probabilities)
-
+    probability_good = 1.0
     #deleting
-    for i in xrange(A):
-        a = A - i
-        if i > 0:
-            probability_good /= probabilities[-i]
+    #i - how much left out of typed messages
+    for i in xrange(A + 1):
+        probability_good *= probabilities[i]
         probability_bad  = 1.0 - probability_good
-        average = probability_good * (B - A + 1 + i*2) +\
-                probability_bad * (2*B - A + 2 + i*2)
-        res = min(res,average)
+
+        penalty_good = (B - i) + 1 + (A - i)
+        penalty_bad  = penalty_good + B + 1
+        average = probability_good * penalty_good +\
+                  probability_bad  * penalty_bad
+        res = min(average,res)
 
     #backspaces
     res = round(res,6)
