522960b49a52106347917044c83d1d90534c52cc
Anton Romanov <theli.ua@gmail.com>
2013-03-21 14:51:03 +0200

    sigh

--- HackerRank/saveprincess.py
+++ HackerRank/saveprincess.py
@@ -4,7 +4,7 @@ MOVES = ['LEFT','RIGHT','UP','DOWN']
 def displayPathtoPrincess(m,grid):
     m,p = (i for i,x in enumerate(grid) if x != '-')
     if grid[m] == 'p': m,p = p,m
-    m,p = ((x/3, x%3) for x in [m,p])
+    m,p = ((x/m, x%m) for x in [m,p])
     if m[0] > p[0]:
         v = 2
     else:
