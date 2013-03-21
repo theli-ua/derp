17a2b4df427111c80ac2efdfef2bb2c9f7c42ba8
Anton Romanov <theli.ua@gmail.com>
2013-03-21 15:02:20 +0200

    oh, ffs

--- HackerRank/saveprincess.py
+++ HackerRank/saveprincess.py
@@ -1,10 +1,10 @@
 #!/bin/python
 # Head ends here
 MOVES = ['LEFT','RIGHT','UP','DOWN']
-def displayPathtoPrincess(m,grid):
+def displayPathtoPrincess(M,grid):
     m,p = (i for i,x in enumerate(grid) if x != '-')
     if grid[m] == 'p': m,p = p,m
-    m,p = ((x/m, x%m) for x in [m,p])
+    m,p = ((x/M, x%M) for x in [m,p])
     if m[0] > p[0]:
         v = 2
     else:
@@ -18,7 +18,7 @@ def displayPathtoPrincess(m,grid):
     path.extend([h] * abs(m[1] - p[1]))
     return '\n'.join([MOVES[p] for p in path])
 # Tail starts here
-m = input()
+m = int(input())
 
 #grid = []
 #for i in xrange(0, m):
