bd475b131c61bf3fe2dc640abf78994699cd3ecc
Anton Romanov <theli.ua@gmail.com>
2013-04-15 17:14:54 +0300

    still slow

--- CodeJam/2013/Q/D.py
+++ CodeJam/2013/Q/D.py
@@ -1,11 +1,16 @@
 from sys import stdin
+from collections import Counter
 
 T = int(stdin.readline())
 
+global bads
+bads = []
+
 def check(keys, visited, chests, path):
-    print 'test path: ' + ' '.join(map(str,path))
     if len(path) == len(chests):
         return path
+    if frozenset(path) in bads: 
+        return None
     for k in xrange(len(chests)):
         if not visited[k]:
             if chests[k][0] in keys:
@@ -16,15 +21,22 @@ def check(keys, visited, chests, path):
                 if res is not None:
                     return res
                 visited[k] = False
+    bads.append(frozenset(path))
     return None
 
 for i in xrange(1, T+1):
+    bads = []
     K,N = map(int, stdin.readline().split())
     keys = map(int, stdin.readline().split())
     chests = [map(int, stdin.readline().split()) for _ in xrange(N)]
     visited = [False] * N
 
-    res = check(keys, visited, chests, [])
+    keys_across_universe = Counter(keys + sum([chest[1:] for chest in chests],[]))
+    required = Counter(chest[0] for chest in chests)
+    if all((required[r] <= keys_across_universe[r] for r in required)):
+        res = check(keys, visited, chests, [])
+    else:
+        res = None
 
     if res is None:
         print 'Case #{0}: IMPOSSIBLE'.format(i)
