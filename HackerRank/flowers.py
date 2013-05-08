cf71752651bea71f7661b67cc3e70fc2798972b4
Anton Romanov <theli.ua@gmail.com>
2013-05-08 17:00:12 +0300

    a bit shorter, lol

--- HackerRank/flowers.py
+++ HackerRank/flowers.py
@@ -1,30 +1,5 @@
 #!/bin/python
+N, K = [int(x) for x in raw_input().split()]
+C = reversed(sorted([int(x) for x in raw_input().split()])[:N])
 
-
-
-
-
-# code snippet illustrating input/output methods 
-N, K = raw_input().split()
-N = int(N)
-K = int(K)
-C = [int(x) for x in raw_input().split()]
-
-HANDS = [[] for _ in xrange(K)]
-curK = 0
-for _ in xrange(N):
-    i = C.index(min(C))
-    HANDS[curK].append(C[i])
-    del C[i]
-    curK = (1 + curK) % K
-
-for i in xrange(K):
-    HANDS[i].sort(reverse = True)
-
-result = 0
-
-for h in HANDS:
-    for i in xrange(len(h)):
-        result += (i+1)*h[i]
-
-print result
+print reduce(lambda x,y: x + (y[0]/K + 1)*y[1], enumerate(C), 0)
