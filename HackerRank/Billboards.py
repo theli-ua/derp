a7e75e6ef3126c1a0040db78efca4f14aba375f6
Anton Romanov <theli.ua@gmail.com>
2013-06-27 18:18:14 +0300

    for variants with same value use only the one with smallest consecutive boards number. now passes :D

--- HackerRank/Billboards.py
+++ HackerRank/Billboards.py
@@ -7,19 +7,20 @@ else:
 N,K = [int(x) for x in input.readline().split()]
 B = [int(input.readline()) for x in xrange(N)]
 
-profits = [ [0] * (K+1) for _ in xrange(2) ]
+prev = [ (0,0) ]
 
 prevmax = 0
 for i in xrange(1, N + 1):
     board = B[i-1]
-    profits[1][0] = prevmax
-    if board == 0:
-        profits[1] = [prevmax] * (K+1)
-    else:
-        for j in xrange(1, K + 1):
-            profit = profits[0][j-1] + board
-            profits[1][j] = profit
-            prevmax = max(profit,prevmax)
-    profits.reverse()
+    cur = [ (0, prevmax) ] 
+    if board != 0:
+        for b in prev:
+            curlen = b[0] + 1
+            value = b[1] + board
+            if value > cur[-1][1]:
+                if curlen < K:
+                    cur.append( ( curlen, value ) )
+                prevmax = max(value, prevmax)
+    prev = cur
 
 print prevmax
